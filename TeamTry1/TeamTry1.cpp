8b01432e8ac02ca58b619afac86c0140a17f0f1e
Joshua Tomasso <thzombiethatllives@gmail.com>
2024-11-11 15:11:17 -0500

    Added loop

--- TeamTry1/TeamTry1.cpp
+++ TeamTry1/TeamTry1.cpp
@@ -5,7 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "Hello World!\n";
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
